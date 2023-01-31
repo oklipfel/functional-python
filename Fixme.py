02911ddd40368b7b091427f72804dd536f4f9233
oklipfe <oklipfe@pitzer.edu>
2023-01-30 20:30:13 -0800

    done!

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,7 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
@@ -20,11 +21,16 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    xs = range(n+1)
+    xs = filter(lambda x: x % 2 == 0, xs)
+    xs = list(xs)
+    return xs
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n inclusive that contain the
+    digit 3.
 
     >>> threes(2)
     []
@@ -37,11 +43,16 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    xs = range(n + 1)
+    xs = filter(lambda x: '3' in str(x), xs)
+    xs = list(xs)
+    return xs
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of all words in the input text that are less than 5
+    characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,6 +66,10 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
+    splittext = text.split()
+    xs = filter(lambda x: len(x) < 5, splittext)
+    xs = list(xs)
+    return xs
 
 
 def squares(n):
@@ -71,12 +86,16 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    s = range(1, n + 1)
+    xs = map(lambda x: x**2, s)
+    xs = list(xs)
+    return xs
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
-
+    Given a list of strings, returns a list of the lengths of the corresponding
+    strings.
     >>> lengths([])
     []
     >>> lengths(['test'])
@@ -84,3 +103,6 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    xs = map(lambda x: len(x), strings)
+    xs = list(xs)
+    return xs
